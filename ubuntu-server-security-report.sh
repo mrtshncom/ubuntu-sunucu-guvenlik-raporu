bdfbd7d5c156521720d25d54b50a261553bbd25d
mrtshncom <130790146+mrtshncom@users.noreply.github.com>
2026-07-15 20:41:05 +0300

    Delete ubuntu-server-security-report.sh

--- ubuntu-server-security-report.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-SINCE="12 hours ago"
-
-echo
-echo "=================================================="
-echo " 1. UFW TARAFINDAN ENGELLENEN TÜM BAĞLANTILAR"
-echo "=================================================="
-
-# Kodun kalan kısmı...
